d4f43476c4b3c7e264bcd5487b0090bd28ef2fbc
baeeunwoo <appe3000@naver.com>
2020-02-13 17:37:56 +0900

    k번째 작은수

--- AA.py
+++ AA.py
@@ -2,15 +2,31 @@
 2-1.k번째 약수
 '''
 
-import sys
+# import sys
 # sys.stdin = open("input.txt", "rt")
-n, k = map(int, input().split())  #띄어쓰기 구분 후 분리해서 읽음
-cnt = 0
-for i in range(1, n + 1):
-    if n % i == 0:
-        cnt += 1
-    if cnt == k:
-        print(i)
-        break
-else:               # 위 for문이 정상작동 했을 때 else로 넘어감
-    print(-1)
+# n, k = map(int, input().split())  #띄어쓰기 구분 후 분리해서 읽음
+# cnt = 0
+# for i in range(1, n + 1):
+#     if n % i == 0:
+#         cnt += 1
+#     if cnt == k:
+#         print(i)
+#         break
+# else:               # 위 for문이 정상작동 했을 때 else로 넘어감
+#     print(-1)
+
+'''
+2-2.k번째 작은수
+'''
+import sys
+sys.stdin = open("input.txt", "rt")
+
+T = int(input())
+for t in range(T):
+    n, s, e, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    a = a [s - 1 : e]
+    print(a[1 : 5])   #1이 두번째 숫자 인덱스1이므로
+    a.sort()
+    print(a)
+    # print("#%d %d" %(t + 1, a[k - 1]))
--- exam1.py
+++ exam1.py
@@ -5,14 +5,22 @@ k번째 약수 연습
 import sys
 sys.stdin = open("input2.txt", "rt")
 
-n, k = map(input().split())
-cnt = 0
-for i in range(1, n + 1):
-    if n % i == 0:
-        cnt += 1
-    if cnt == k:
-        print(i) # 여기를 잘 모르겠 다시 들어보기
-        break
+# n, k = map(int, input().split())
+# cnt = 0
+# for i in range(1, n + 1):
+#     if n % i == 0:
+#         cnt += 1
+#     if cnt == k:  # k번째 작은 수
+#         print(i)  # 여기를 잘 모르겠 다시 들어보기
+#         break
+#
+# else:
+#     print(-1)
 
-else:
-    print(-1)
+'''
+k번째 작은 수    #문제 이해가 잘 안된다.
+'''
+
+T = int(input())
+n, s, e, k = map(int, input().split())
+a = list(map(int, input().split()))
