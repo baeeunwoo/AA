a3d078b2b923f64c94783092a16fe7c8e92412f0
baeeunwoo <appe3000@naver.com>
2020-02-13 16:16:28 +0900

    k번째 약수

--- AA.py
+++ AA.py
@@ -1,3 +1,16 @@
+'''
+2-1.k번째 약수
+'''
+
 import sys
-sys.stdin = open("input.txt", "rt")
-n =
+# sys.stdin = open("input.txt", "rt")
+n, k = map(int, input().split())  #띄어쓰기 구분 후 분리해서 읽음
+cnt = 0
+for i in range(1, n + 1):
+    if n % i == 0:
+        cnt += 1
+    if cnt == k:
+        print(i)
+        break
+else:
+    print(-1)
