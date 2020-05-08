82f8cfb247724fba6d1e8473884db953d59e88f6
baeeunwoo <appe3000@naver.com>
2020-05-08 11:36:06 +0900

    사과나무(다이아몬드)

--- Search/sudoku.py
+++ Search/sudoku.py
@@ -2,6 +2,7 @@
 2-10. 스도쿠 검사
 '''
 
+
 # ?? 왜 답이 NO로 나오나
 # ch: 체크리스트 3개 필요 , 행 열 그룹 ch1 ch2 ch3
 # ch1 [a[i][j]] = 1  : 0번행탐색 (가로)
@@ -12,13 +13,13 @@
 
 # 왜 0은 안쓴다 하신건지? ch 설명하실때 리스트가 1~9만 있었음
 
-def check(a):
+def check(b):
     for i in range(9):
         ch1 = [0] * 10
         ch2 = [0] * 10
         for j in range(9):
-            ch1[a[i][j]] = 1
-            ch2[a[j][i]] = 1
+            ch1[b[i][j]] = 1
+            ch2[b[j][i]] = 1
         if sum(ch1) != 9 or sum(ch2) != 9:
             return False
     for i in range(3):  # 0 1 2
@@ -26,7 +27,7 @@ def check(a):
             ch3 = [0] * 10  # 초기화
             for k in range(3):
                 for s in range(3):
-                    ch3[a[i * 3 + k][j * 3 + s]] = 1
+                    ch3[b[i * 3 + k][j * 3 + s]] = 1
                 if sum(ch3) != 9:  # 9개인지 체크
                     return False  # 면 종료
         return True  # 중복이 없었다. (myThink:4중for문의 true라 젤 위 for 밑 한칸 들여쓰기?)
