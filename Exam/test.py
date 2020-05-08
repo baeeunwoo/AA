f9b17954a92656210075c19435456ea899460d3a
baeeunwoo <appe3000@naver.com>
2020-05-08 11:35:12 +0900

    사과나무(다이아몬드)

---
+++ Exam/test.py
@@ -0,0 +1,31 @@
+
+'''
+from calculator import sum
+
+print(sum(3, 5))
+
+from calculator import sum, difference, product, square
+
+
+
+from calculator import *
+
+print(sum(3, 5))
+print(difference(3, 5))
+print(product(3, 5))
+print(square(3))
+
+
+
+from random import randint
+
+# a <= N <=b인, 랜덤한 정수(난수) N의 값을 리턴시켜줌
+x = randint(1, 20)
+print(x)
+
+'''
+
+from random import uniform
+
+x = uniform(0, 1)
+print(x)
